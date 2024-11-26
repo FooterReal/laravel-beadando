29d446ca58d5c81a40dcad27e227c57c900ae232
FooterReal <footerreal@gmail.com>
2024-11-26 21:10:14 +0100

    User creation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -50,7 +50,29 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'pos' => 'required|exists:positions,id',
+            'email' => 'required|email',
+            'password' => 'required',
+            'phone' => 'required',
+            'card' => 'required|min:16|max:16|regex:/[a-zA-Z0-9]+/i'
+        ],
+        [
+            'required' => 'This field is not optional: :attribute'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'email_verified_at' => now(),
+            'password' => password_hash($request->password, PASSWORD_DEFAULT),
+            'phone_number' => $request->phone,
+            'card_number' => $request->card,
+            'position_id' => $request->pos
+        ]);
+
+        return redirect()->route('users.index');
     }
 
     /**
